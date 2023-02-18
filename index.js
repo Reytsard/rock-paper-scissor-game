7bd96e17700aa932fa1757ea27d705bb31dd5f54
Rey <ronaldreyjr061201@gmail.com>
2023-02-18 12:21:07 +0800

    added function getComputerChoice

---
+++ index.js
@@ -0,0 +1,35 @@
+/*Rock Paper Scissor game
+first: we need to prompt a greeting for the game 
+second: we should make the side of the computer to have a choice
+third: we should prompt the user to have a choice
+
+Pseudocode:
+The game will start
+the computer will pick a choice
+the user will picj a choice
+if computer's choice is better than user's choice
+display that the user lose
+else if users choice is better than the computer
+display that the user won
+if compyter and user have the same choice
+display that it is a time
+
+picking of the choice
+roll for computer: we can use number to pick what choice 
+1 -> Rock
+2 -> Paper
+3 -> Scissor 
+*/
+
+//computerRoll
+function getComputerChoice(){
+    let a = Math.floor(Math.random() * 3 + 1);
+    switch(a){
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissor";
+    }
+}
+
+let userChoice = prompt("Rock Paper Scissor Shoot!\nPick your Choice:");
+
